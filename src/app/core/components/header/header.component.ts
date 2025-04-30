a6f559bdaac67b4385caa1bde9eb484069fd8773
Rafael Soriano <raansopro324@gmail.com>
2025-04-30 11:50:27 -0500

    Logout y correo del usuario logeado

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AvatarModule } from 'primeng/avatar';
@@ -6,6 +6,9 @@ import { InputTextModule } from 'primeng/inputtext';
 import { MenuModule } from 'primeng/menu';
 import { ButtonModule } from 'primeng/button';
 import { MenuItem } from 'primeng/api';
+import { Router } from '@angular/router';
+import { ApiClientService } from '../../api/httpclient';
+
 @Component({
   selector: 'app-header',
   imports: [CommonModule,
@@ -17,32 +20,33 @@ import { MenuItem } from 'primeng/api';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  private router = inject(Router);
   @Input() pageTitle: string = 'Gestión de Seguros'; // Título dinámico
   @Input() pageSubtitle: string = 'Administra y revisa todas las pólizas de seguros'; // Subtítulo dinámico
 
-  searchQuery: string = ''; // Valor de la búsqueda
+  searchQuery: string = '';
 
   user = {
-    name: 'Carlos Mendoza',
-    role: 'Agente de Seguros',
+    name: 'Usuario',
     icon: 'pi pi-user',
   };
 
   menuItems: MenuItem[] = [
-
     { label: 'Cerrar Sesión', icon: 'pi pi-sign-out', command: () => this.logout() }
   ];
 
-  navigateToProfile() {
-    console.log('Navegando al perfil');
-  }
+  constructor(private apiClient: ApiClientService) { }
 
-  navigateToSettings() {
-    console.log('Navegando a configuración');
+  ngOnInit() {
+    const email = this.apiClient.getCurrentUserEmail();
+    if (email) {
+      this.user.name = email;
+    }
   }
 
   logout() {
-    console.log('Cerrando sesión');
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
   }
 }
