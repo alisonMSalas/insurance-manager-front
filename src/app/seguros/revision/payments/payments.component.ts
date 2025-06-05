9f83a6ebab3875cf479ec680088032458dd16e9b
Alison <alisonsalas0319@gmail.com>
2025-06-05 00:56:33 -0500

    Agregar componente de pagos y funcionalidad de rechazo de documentos; actualizar estilos y lógica de formularios

---
+++ src/app/seguros/revision/payments/payments.component.ts
@@ -0,0 +1,65 @@
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { ContratacionesService } from '../../../core/services/contrataciones.service';
+import { Contract } from '../../../shared/interfaces/contract';
+import { Insurance } from '../../../shared/interfaces/insurance';
+import { Client } from '../../../shared/interfaces/client';
+import { PaymentsService } from '../../service/payments.service';
+import { ButtonModule } from 'primeng/button';
+import { PaymentUrl } from '../../../shared/interfaces/payment-url-dto';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
+import { Pipe } from '@angular/core';
+
+@Component({
+  selector: 'app-payments',
+  imports: [ButtonModule,CardModule,CommonModule],
+  templateUrl: './payments.component.html',
+  styleUrl: './payments.component.css'
+})
+export class PaymentsComponent implements OnInit {
+  @Input() contratoId: string = '';
+
+  contractService = inject(ContratacionesService);
+  paymentsService = inject(PaymentsService)
+  contract: Contract | null = null;
+
+  policy: Insurance | null = null;
+  client: Client | null = null;
+
+
+  ngOnInit(): void {
+    this.getContractById();
+  }
+  getContractById() {
+    if (!this.contratoId) {
+      console.warn('No se recibió contratoId como @Input');
+      return;
+    }
+
+    this.contractService.getById(this.contratoId).subscribe({
+      next: (data) => {
+        console.log('Contrato recibido:', data);
+        this.contract = data;
+        this.policy = data.insurance!;
+        this.client = data.client!;
+
+      },
+      error: (error) => {
+        console.error('Error al obtener el contrato:', error);
+      }
+    });
+  }
+
+  iniciarPago() {
+    this.paymentsService.createCheckoutSession(this.contratoId)
+      .subscribe({
+        next: (paymentUrl: PaymentUrl) => window.location.href = paymentUrl.url,
+        error: err => console.error('Error al iniciar sesión de pago', err)
+      });
+  }
+  getStatusClass(status: string): string {
+  // Normaliza el nombre del estado para evitar problemas con mayúsculas/espacios
+  const normalizedStatus = status.toLowerCase().trim().replace(/\s+/g, '-');
+  return `status-${normalizedStatus}`;
+}
+}
